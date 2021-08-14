b37ae1a21f881f32ef157dcf61b61141550ee4ed
Preeti2105 <85576938+Preeti2105@users.noreply.github.com>
2021-08-14 11:40:12 +0530

    Add files via upload

---
+++ calculator by python.py
@@ -0,0 +1,21 @@
+#GUI development using tkinter
+##class Abc():
+##    a=10
+##    def fun(self,v):
+##        print(self.a)
+##        print('hello')
+
+##import tkinter as t
+##app = t.Tk()
+##app.title('arithemetic tool')
+##app.geometry('280x150')
+
+##t.Label(app, text = 'hello').pack()
+##t.Label(app, text = 'bye').pack()
+##t.Label(app, text = 'hi').pack()
+##
+##
+##t.Label(app, text = 'hello').place()(x=10, y=20)
+##t.Label(app, text = 'bye').place()(x=30, y=40)
+##t.Label(app, text = 'hi').place()(x=50, y=60)
+
